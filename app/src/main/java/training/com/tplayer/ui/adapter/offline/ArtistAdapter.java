9f9d295b9f9d5b2b8230f638164d4cfb8fd24d36
NguyenHuuTho <huutho.snova@gmail.com>
2017-04-28 19:10:25 +0700

    - added view all tab ofline

---
+++ app/src/main/java/training/com/tplayer/ui/adapter/offline/ArtistAdapter.java
@@ -0,0 +1,8 @@
+package training.com.tplayer.ui.adapter.offline;
+
+/**
+ * Created by hnc on 28/04/2017.
+ */
+
+public class ArtistAdapter {
+}
---
+++ app/src/main/java/training/com/tplayer/ui/entity/offline/FolderEntity.java
@@ -0,0 +1,8 @@
+package training.com.tplayer.ui.entity.offline;
+
+/**
+ * Created by hnc on 28/04/2017.
+ */
+
+public class FolderEntity {
+}
